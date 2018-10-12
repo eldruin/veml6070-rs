d3fd2d48c449e1fbbd80d1ba2484124e300bbc7e
Diego Barrios Romero <eldruin@gmail.com>
2018-10-12 18:30:53 +0200

    Add building release and examples

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,8 @@ set -euxo pipefail
 
 main() {
     cargo check --target $TARGET
+    cargo build --target $TARGET --release
+    cargo build --target $TARGET --examples
 
     if [ $TRAVIS_RUST_VERSION = nightly ]; then
         cargo test --target $TARGET
