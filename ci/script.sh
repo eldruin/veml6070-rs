312fabfa891eeedda82f68959d72882d151ca08e
Diego Barrios Romero <eldruin@gmail.com>
2018-10-16 21:22:17 +0200

    Fix variable name

--- ci/script.sh
+++ ci/script.sh
@@ -1,24 +1,24 @@
 set -euxo pipefail
 
 main() {
-    if [[ ! $string =~ .*linux.* ]]; then
+    if [[ ! $TARGET =~ .*linux.* ]]; then
         sed -i "s/linux-embedded-hal/#linux-embedded-hal/g" Cargo.toml
         sed -i "s/embedded-hal-mock/#embedded-hal-mock/g" Cargo.toml
     fi
 
     cargo check --target $TARGET
     cargo build --target $TARGET --release
-    if [[ $string =~ .*linux.* ]]; then
+    if [[ $TARGET =~ .*linux.* ]]; then
         cargo build --target $TARGET --examples
     fi
 
     if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        if [[ $string =~ .*linux.* ]]; then
+        if [[ $TARGET =~ .*linux.* ]]; then
             cargo test --target $TARGET
         fi
     fi
 
-    if [[ ! $string =~ .*linux.* ]]; then
+    if [[ ! $TARGET =~ .*linux.* ]]; then
         sed -i "s/#linux-embedded-hal/linux-embedded-hal/g" Cargo.toml
         sed -i "s/#embedded-hal-mock/embedded-hal-mock/g" Cargo.toml
     fi
