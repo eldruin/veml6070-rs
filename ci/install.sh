97dd93ef30df9c6277b03d5827f41017e1f4abb5
Diego Barrios Romero <eldruin@gmail.com>
2018-10-16 22:02:42 +0200

    Remove unnecessary steps

--- ci/install.sh
+++ ci/install.sh
@@ -8,10 +8,6 @@ main() {
     else
         echo "Target $TARGET is already installed"
     fi
-    source ~/.cargo/env || true
-    mv .cargo ._cargo
-    cargo install cargo-travis || true
-    mv ._cargo .cargo
 }
 
 main
