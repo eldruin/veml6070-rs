d00df959dd745dccfa7e9c558e6c991a59418a6e
Diego Barrios Romero <eldruin@gmail.com>
2018-10-16 21:18:07 +0200

    Add more targets, build examples and run tests only on linux targets

--- ci/script.sh
+++ ci/script.sh
@@ -1,12 +1,26 @@
 set -euxo pipefail
 
 main() {
+    if [[ ! $string =~ .*linux.* ]]; then
+        sed -i "s/linux-embedded-hal/#linux-embedded-hal/g" Cargo.toml
+        sed -i "s/embedded-hal-mock/#embedded-hal-mock/g" Cargo.toml
+    fi
+
     cargo check --target $TARGET
     cargo build --target $TARGET --release
-    cargo build --target $TARGET --examples
+    if [[ $string =~ .*linux.* ]]; then
+        cargo build --target $TARGET --examples
+    fi
 
     if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        cargo test --target $TARGET
+        if [[ $string =~ .*linux.* ]]; then
+            cargo test --target $TARGET
+        fi
+    fi
+
+    if [[ ! $string =~ .*linux.* ]]; then
+        sed -i "s/#linux-embedded-hal/linux-embedded-hal/g" Cargo.toml
+        sed -i "s/#embedded-hal-mock/embedded-hal-mock/g" Cargo.toml
     fi
 }
 
