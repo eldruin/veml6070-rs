bf54454abb2acac6b89b333f981c145d81d4637f
Diego Barrios Romero <eldruin@gmail.com>
2018-10-16 21:53:47 +0200

    Enhance installation step

--- ci/install.sh
+++ ci/install.sh
@@ -1,9 +1,17 @@
 set -euxo pipefail
 
 main() {
-    if [ $TARGET != x86_64-unknown-linux-gnu ]; then
+    rustup component add rust-src
+    SYSROOT=$(rustc --print sysroot)
+    if [[ ! "$SYSROOT" =~ "$TARGET" ]]; then
         rustup target add $TARGET
+    else
+        echo "Target $TARGET is already installed"
     fi
+    source ~/.cargo/env || true
+    mv .cargo ._cargo
+    cargo install cargo-travis || true
+    mv ._cargo .cargo
 }
 
 main
